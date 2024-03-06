6ab781f0c42829b58ab07794f592c20760d4f96f
José Manuel Hernández M <manuelmayorga99@gmail.com>
2024-03-05 21:41:49 -0600

    Se crea la estructura de carpetas para el proyecto de Pokemon

---
+++ pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>¿Quien es este pokemon?</h1>
+    <PokemonPicture></PokemonPicture>
+    <PokemonOptions></PokemonOptions>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue';
+import PokemonOptions from '@/components/PokemonOptions.vue';
+export default {
+    name: 'PokemonPage',
+    components: {
+        PokemonPicture,
+        PokemonOptions
+    }
+}
+</script>
