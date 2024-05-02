fb6d747bad4856d1f094f4f88648b6737ea305ef
José Manuel Hernández M <manuelmayorga99@gmail.com>
2024-05-01 21:43:06 -0600

    Se implementa lazy load en rutas

--- rutas-ciclo/src/router/router.js
+++ rutas-ciclo/src/router/router.js
@@ -1,16 +1,16 @@
 import { createRouter, createWebHashHistory } from "vue-router"
-import AboutPage from "@/modules/pokemon/pages/AboutPage.vue"
-import ListPage from "@/modules/pokemon/pages/ListPage.vue"
-import PokemonPage from "@/modules/pokemon/pages/PokemonPage.vue"
-import NoPageFound from "@/modules/shared/pages/NoPageFound.vue"
+//import AboutPage from "@/modules/pokemon/pages/AboutPage.vue"
+// import ListPage from "@/modules/pokemon/pages/ListPage.vue"
+// import PokemonPage from "@/modules/pokemon/pages/PokemonPage.vue"
+// import NoPageFound from "@/modules/shared/pages/NoPageFound.vue"
 
 
 //Definición de rutas
 const routes = [
-    {path: '/', component: ListPage},
-    {path: '/about', component: AboutPage},
-    {path: '/id', component: PokemonPage},
-    {path: '/:pathMatch(.*)*', component: NoPageFound}
+    {path: '/', component: () => import(/*webpackChunkName: "ListPage"*/ '@/modules/pokemon/pages/ListPage')},
+    {path: '/about', component: () => import(/*webpackChunkName: "AboutPage"*/ '@/modules/pokemon/pages/AboutPage')},
+    {path: '/id', component: () => import(/*webpackChunkName: "PokemonPage"*/ '@/modules/pokemon/pages/PokemonPage')},
+    {path: '/:pathMatch(.*)*', component: () => import(/*webpackChunkName: "NotFoundPage"*/ '@/modules/shared/pages/NoPageFound')}
 ]
 
 //Configurar modo historia
